4584013c3e2b6b5ea667afbc485eec7d7188ca82
Ahmed Raza <hafizahmedattari@gmail.com>
2016-02-17 08:02:47 +0500

    Initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by Raza on 2/17/2016.
+ */
+
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular 2 App</h1>'
+})
+export class AppComponent {}
